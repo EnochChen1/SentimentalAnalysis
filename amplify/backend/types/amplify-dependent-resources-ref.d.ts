d14f41e45a24453875103ea224e942955b5724e7
EnochChen1 <enoch.chen.personal@gmail.com>
2024-04-28 18:14:26 -0700

    added amplify info for authorization

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "sentimentalanalysis2643c946": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import "@aws-amplify/ui-react/styles.css"
+import { Amplify } from "aws-amplify";
+import aws_exports from './aws-exports'
 
-function App() {
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
+
+Amplify.configure(aws_exports);
+
+
+function App({signOut}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello from V2
-        </p>
-        
-      </header>
-    </div>
+    <View className = "App">
+      <Card>
+        <Image src={logo} className="App-logo" alt="logo" />
+        <Heading level={1}> We now have Auth! </Heading>
+      </Card>
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
