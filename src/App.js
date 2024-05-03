8120a7c4035db96dd6c5d187b081040b7547f948
EnochChen1 <enoch.chen.personal@gmail.com>
2024-05-02 23:38:45 -0700

    hopefully this update works

--- src/App.js
+++ src/App.js
@@ -16,26 +16,33 @@ import {
 
 const [sentence, setSentence] = useState(null);
 const [sentiment, setSentiment] = useState(null);
+const api = "https://eglsgolhn6.execute-api.us-east-1.amazonaws.com/dev/sentiment_analysis";
+
 
 function handleSubmit(e) {
   e.preventDefault();
   setSentence(e.currentTarget.sentence.value);
 
-  const api = "https://eglsgolhn6.execute-api.us-east-1.amazonaws.com/dev/sentiment_analysis";
+  fetchSentiment(sentence);
 
-  React.useEffect(() => {
-    $.get(api, {
-      sentence: sentence
-    },
-    function (sentiment, status) {
-      setSentiment = sentiment;
-      console.log(status+" "+sentiment);
-    });
-    }, []);
+
+
+  
 }
   
-  
 
+export function fetchSentiment(sentence) {
+  var request = fetch(api, {
+    method:'GET',
+    headers: {
+      'Content-Type':'application/json',
+      'sentence':sentence
+    }
+  })
+  .then(response => response.json())
+  .then((data) => {return data;})
+  .catch(error => console.log("Error while fetching", error));
+}
 
 
 function App({signOut}) {
